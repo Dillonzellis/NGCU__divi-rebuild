3055a592e47a2863b60b4d0d46d8969a3afa99e4
Dillon Ellis <dillonzellis@gmail.com>
2022-12-06 16:56:40 -0500

    start home page green cta

--- GBD_Theme/page-templates/header.php
+++ GBD_Theme/page-templates/header.php
@@ -4,20 +4,20 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>" />
     <?php
-    elegant_description();
-    elegant_keywords();
-    elegant_canonical();
+elegant_description();
+elegant_keywords();
+elegant_canonical();
 
-    /**
-     * Fires in the head, before {@see wp_head()} is called. This action can be used to
-     * insert elements into the beginning of the head before any styles or scripts.
-     *
-     * @since 1.0
-     */
-    do_action('et_head_meta');
+/**
+ * Fires in the head, before {@see wp_head()} is called. This action can be used to
+ * insert elements into the beginning of the head before any styles or scripts.
+ *
+ * @since 1.0
+ */
+do_action('et_head_meta');
 
-    $template_directory_uri = get_template_directory_uri();
-    ?>
+$template_directory_uri = get_template_directory_uri();
+?>
 
     <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 
@@ -34,34 +34,34 @@
 
 <body <?php body_class(); ?>>
     <?php
-    wp_body_open();
+wp_body_open();
 
-    $product_tour_enabled = et_builder_is_product_tour_enabled();
-    $page_container_style = $product_tour_enabled ? ' style="padding-top: 0px;"' : ''; ?>
+$product_tour_enabled = et_builder_is_product_tour_enabled();
+$page_container_style = $product_tour_enabled ? ' style="padding-top: 0px;"' : ''; ?>
     <div id="page-container" <?php echo et_core_intentionally_unescaped($page_container_style, 'fixed_string'); ?>>
         <?php
-        if ($product_tour_enabled || is_page_template('page-template-blank.php')) {
-            return;
-        }
+if ($product_tour_enabled || is_page_template('page-template-blank.php')) {
+    return;
+}
 
-        $et_secondary_nav_items = et_divi_get_top_nav_items();
+$et_secondary_nav_items = et_divi_get_top_nav_items();
 
-        $et_phone_number = $et_secondary_nav_items->phone_number;
+$et_phone_number = $et_secondary_nav_items->phone_number;
 
-        $et_email = $et_secondary_nav_items->email;
+$et_email = $et_secondary_nav_items->email;
 
-        $et_contact_info_defined = $et_secondary_nav_items->contact_info_defined;
+$et_contact_info_defined = $et_secondary_nav_items->contact_info_defined;
 
-        $show_header_social_icons = $et_secondary_nav_items->show_header_social_icons;
+$show_header_social_icons = $et_secondary_nav_items->show_header_social_icons;
 
-        $et_secondary_nav = $et_secondary_nav_items->secondary_nav;
+$et_secondary_nav = $et_secondary_nav_items->secondary_nav;
 
-        $et_top_info_defined = $et_secondary_nav_items->top_info_defined;
+$et_top_info_defined = $et_secondary_nav_items->top_info_defined;
 
-        $et_slide_header = 'slide' === et_get_option('header_style', 'left') || 'fullscreen' === et_get_option('header_style', 'left') ? true : false;
+$et_slide_header = 'slide' === et_get_option('header_style', 'left') || 'fullscreen' === et_get_option('header_style', 'left') ? true : false;
 
-        $show_search_icon = (false !== et_get_option('show_search_icon', true) && !$et_slide_header) || is_customize_preview();
-        ?>
+$show_search_icon = (false !== et_get_option('show_search_icon', true) && !$et_slide_header) || is_customize_preview();
+?>
 
         <?php if ($et_top_info_defined && !$et_slide_header || is_customize_preview()): ?>
         <?php ob_start(); ?>
@@ -84,56 +84,56 @@
                     <?php endif; ?>
 
                     <?php
-                if (true === $show_header_social_icons) {
-                    get_template_part('includes/social_icons', 'header');
-                } ?>
+            if (true === $show_header_social_icons) {
+                get_template_part('includes/social_icons', 'header');
+            } ?>
                 </div>
 
                 <?php endif; // true === $et_contact_info_defined ?>
 
 				<div id="et-secondary-menu">
 				<?php
-            if (!$et_contact_info_defined && true === $show_header_social_icons) {
-                get_template_part('includes/social_icons', 'header');
-            } else if ($et_contact_info_defined && true === $show_header_social_icons) {
-                ob_start();
+        if (!$et_contact_info_defined && true === $show_header_social_icons) {
+            get_template_part('includes/social_icons', 'header');
+        } else if ($et_contact_info_defined && true === $show_header_social_icons) {
+            ob_start();
 
-                get_template_part('includes/social_icons', 'header');
+            get_template_part('includes/social_icons', 'header');
 
-                $duplicate_social_icons = ob_get_contents();
+            $duplicate_social_icons = ob_get_contents();
 
-                ob_end_clean();
+            ob_end_clean();
 
-                printf(
-                    '<div class="et_duplicate_social_icons">
+            printf(
+                '<div class="et_duplicate_social_icons">
 								%1$s
 							</div>',
-                    et_core_esc_previously($duplicate_social_icons)
-                );
-            }
+                et_core_esc_previously($duplicate_social_icons)
+            );
+        }
 
-            if ('' !== $et_secondary_nav) {
-                echo et_core_esc_wp($et_secondary_nav);
-            }
+        if ('' !== $et_secondary_nav) {
+            echo et_core_esc_wp($et_secondary_nav);
+        }
 
-            et_show_cart_total();
+        et_show_cart_total();
                 ?>
             </div>
 
         </div>
     </div>
     <?php
-            $top_header = ob_get_clean();
+        $top_header = ob_get_clean();
 
-            /**
-             * Filters the HTML output for the top header.
-             *
-             * @since 3.10
-             *
-             * @param string $top_header
-             */
-            echo et_core_intentionally_unescaped(apply_filters('et_html_top_header', $top_header), 'html');
-        ?>
+        /**
+         * Filters the HTML output for the top header.
+         *
+         * @since 3.10
+         *
+         * @param string $top_header
+         */
+        echo et_core_intentionally_unescaped(apply_filters('et_html_top_header', $top_header), 'html');
+    ?>
     <?php endif; // true ==== $et_top_info_defined ?>
 
 	<?php if ($et_slide_header || is_customize_preview()): ?>
@@ -157,7 +157,7 @@
         }
 
         et_show_cart_total();
-                ?>
+            ?>
                 <?php if (false !== et_get_option('show_search_icon', true) || is_customize_preview()): ?>
                 <?php if ('fullscreen' !== et_get_option('header_style', 'left')) { ?>
                 <div class="clear"></div>
@@ -214,7 +214,7 @@
 
         $slide_nav = wp_nav_menu(array('theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'echo' => false, 'items_wrap' => '%3$s'));
         $slide_nav .= wp_nav_menu(array('theme_location' => 'secondary-menu', 'container' => '', 'fallback_cb' => '', 'echo' => false, 'items_wrap' => '%3$s'));
-            ?>
+                ?>
 
             <ul id="mobile_menu_slide" class="<?php echo esc_attr($slide_menu_class); ?>">
 
@@ -222,8 +222,7 @@
         if ('' === $slide_nav):
                 ?>
                 <?php if ('on' === et_get_option('divi_home_link')) { ?>
-                <li <?php if (is_home())
-                    echo ('class="current_page_item"'); ?>><a
+                <li <?php if (is_home()) echo ('class="current_page_item"'); ?>><a
                         href="<?php echo esc_url(home_url('/')); ?>">
                         <?php esc_html_e('Home', 'Divi'); ?>
                     </a></li>
@@ -299,43 +298,42 @@
                 <?php if (!$et_slide_header || is_customize_preview()): ?>
                 <nav id="top-menu-nav">
                     <?php
-                    $menuClass = 'nav';
-                    if ('on' === et_get_option('divi_disable_toptier'))
-                        $menuClass .= ' et_disable_top_tier';
-                    $primaryNav = '';
+                        $menuClass = 'nav';
+                        if ('on' === et_get_option('divi_disable_toptier'))
+                            $menuClass .= ' et_disable_top_tier';
+                        $primaryNav = '';
 
-                    $primaryNav = wp_nav_menu(array('theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false));
-                    if (empty($primaryNav)):
-                    ?>
+                        $primaryNav = wp_nav_menu(array('theme_location' => 'primary-menu', 'container' => '', 'fallback_cb' => '', 'menu_class' => $menuClass, 'menu_id' => 'top-menu', 'echo' => false));
+                        if (empty($primaryNav)):
+                        ?>
                     <ul id="top-menu" class="<?php echo esc_attr($menuClass); ?>">
                         <?php if ('on' === et_get_option('divi_home_link')) { ?>
-                        <li <?php if (is_home())
-                                echo ('class="current_page_item"'); ?>><a
+                        <li <?php if (is_home()) echo ('class="current_page_item"'); ?>><a
                                 href="<?php echo esc_url(home_url('/')); ?>">
                                 <?php esc_html_e('Home', 'Divi'); ?>
                             </a></li>
                         <?php }
-                        ; ?>
+                            ; ?>
 
                         <?php show_page_menu($menuClass, false, false); ?>
                         <?php show_categories_menu($menuClass, false); ?>
                     </ul>
                     <?php else:
-                        echo et_core_esc_wp($primaryNav);
-                    endif;
-                    ?>
+                            echo et_core_esc_wp($primaryNav);
+                        endif;
+                        ?>
                 </nav>
                 <?php endif; ?>
 
                 <?php
-                if (!$et_top_info_defined && (!$et_slide_header || is_customize_preview())) {
-                    et_show_cart_total(
-                        array(
-                            'no_text' => true,
-                        )
-                    );
-                }
-                ?>
+                    if (!$et_top_info_defined && (!$et_slide_header || is_customize_preview())) {
+                        et_show_cart_total(
+                            array(
+                                'no_text' => true,
+                            )
+                        );
+                    }
+                    ?>
 
                 <?php if ($et_slide_header || is_customize_preview()): ?>
                 <span
@@ -350,14 +348,14 @@
 
                 <?php
 
-                /**
-                 * Fires at the end of the 'et-top-navigation' element, just before its closing tag.
-                 *
-                 * @since 1.0
-                 */
-                do_action('et_header_top');
+                    /**
+                     * Fires at the end of the 'et-top-navigation' element, just before its closing tag.
+                     *
+                     * @since 1.0
+                     */
+                    do_action('et_header_top');
 
-                ?>
+                    ?>
             </div> <!-- #et-top-navigation -->
         </div> <!-- .container -->
         <?php if ($show_search_icon): ?>
@@ -365,19 +363,19 @@
             <div class="container et_search_form_container">
                 <form role="search" method="get" class="et-search-form" action="<?php echo esc_url(home_url('/')); ?>">
                     <?php
-            printf(
-                '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
-                esc_attr__('Search &hellip;', 'Divi'),
-                get_search_query(),
-                esc_attr__('Search for:', 'Divi')
-            );
+                printf(
+                    '<input type="search" class="et-search-field" placeholder="%1$s" value="%2$s" name="s" title="%3$s" />',
+                    esc_attr__('Search &hellip;', 'Divi'),
+                    get_search_query(),
+                    esc_attr__('Search for:', 'Divi')
+                );
 
-            /**
-             * Fires inside the search form element, just before its closing tag.
-             *
-             * @since ??
-             */
-            do_action('et_search_form_fields');
+                /**
+                 * Fires inside the search form element, just before its closing tag.
+                 *
+                 * @since ??
+                 */
+                do_action('et_search_form_fields');
                     ?>
                 </form>
                 <span class="et_close_search_field"></span>
@@ -399,9 +397,9 @@
     ?>
     <div id="et-main-area">
         <?php
-        /**
-         * Fires after the header, before the main content is output.
-         *
-         * @since 3.10
-         */
-        do_action('et_before_main_content');
+    /**
+     * Fires after the header, before the main content is output.
+     *
+     * @since 3.10
+     */
+    do_action('et_before_main_content');
